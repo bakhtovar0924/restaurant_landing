18a07f7b957567c5997258303f9b8c7a4d49cf57
Bakhtovar ⭐ <bakhtovar0924@gmail.com>
2026-06-30 05:50:05 +0500

    fix bag path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/restaurant-landing/',
+  base: '/restaurant_landing/',
   plugins: [react(), tailwindcss()],
 })
